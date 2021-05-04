5ffb578e4e917aa006171a2b16dc783ca12bfe87
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-04 17:17:58 +0300

    Create player controller class

---
+++ Source/ShootThemUp/Private/Characters/Controllers/STUPlayerController.cpp
@@ -0,0 +1,5 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+
+#include "Characters/Controllers/STUPlayerController.h"
+
---
+++ Source/ShootThemUp/Public/Characters/Controllers/STUPlayerController.h
@@ -0,0 +1,17 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "STUPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API ASTUPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
