1c0a826fb4934d12b15d5d91b12079006b8ac191
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-04 16:27:55 +0300

    Convert project to C++

---
+++ Source/ShootThemUp/ShootThemUp.cpp
@@ -0,0 +1,6 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#include "ShootThemUp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShootThemUp, "ShootThemUp" );
---
+++ Source/ShootThemUp/ShootThemUp.h
@@ -0,0 +1,6 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
