5643061cc8634147cd5b5cc5f2eb569de79e848e
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-09 22:04:55 +0300

    Create damage test actors

---
+++ Source/ShootThemUp/Private/Dev/STUDevDamageActor.cpp
@@ -0,0 +1,35 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+
+#include "Dev/STUDevDamageActor.h"
+#include "DrawDebugHelpers.h"
+#include "Kismet/GameplayStatics.h"
+
+ASTUDevDamageActor::ASTUDevDamageActor()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    SceneComponent = CreateDefaultSubobject<USceneComponent>("SceneComponent");
+    SetRootComponent(SceneComponent);
+}
+
+void ASTUDevDamageActor::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ASTUDevDamageActor::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    const FVector ActorLocation = GetActorLocation();
+    DrawDebugSphere(GetWorld(), ActorLocation, Radius, 24, SphereColor);
+    UGameplayStatics::ApplyRadialDamage(GetWorld(),
+                                        Damage,
+                                        ActorLocation,
+                                        Radius,
+                                        nullptr,
+                                        {},
+                                        this,
+                                        nullptr,
+                                        bDoFullDamage);
+}
---
+++ Source/ShootThemUp/Public/Dev/STUDevDamageActor.h
@@ -0,0 +1,34 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUDevDamageActor.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUDevDamageActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ASTUDevDamageActor();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Radius = 300.f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FColor SphereColor = FColor::Red;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Damage = 1.f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    bool bDoFullDamage = false;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
+    USceneComponent* SceneComponent = nullptr;
+};
