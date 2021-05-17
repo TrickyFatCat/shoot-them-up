e517205abac7d4a6a31e01b0c520ab1d6aca30f8
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-17 20:14:47 +0300

    Create new resource class

---
+++ Source/ShootThemUp/Private/Objects/Resource.cpp
@@ -0,0 +1,170 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+
+#include "Resource.h"
+
+#include "Math/UnitConversion.h"
+
+void UResource::SetValue(const float NewValue)
+{
+	if (NewValue < 0.f || Value <= 0.f) return;
+
+	Value = NewValue;
+}
+
+void UResource::SetValueMax(const float NewValue)
+{
+	if (NewValue <= 0.f) return;
+
+	ValueMax = NewValue;
+}
+
+void UResource::DecreaseValue(const float DeltaValue, const bool bClampToZero)
+{
+	if (DeltaValue <= 0.f) return;
+
+	Value -= DeltaValue;
+
+	if (bClampToZero)
+	{
+		Value = FMath::Max(Value - DeltaValue, 0.f);
+	}
+
+	if (bAutoIncreaseEnabled)
+	{
+		StartAutoIncreaseDelay();
+	}
+
+	OnValueDecreased.Broadcast(Value);
+}
+
+void UResource::IncreaseValue(const float DeltaValue, const bool bClampToMax)
+{
+	if (DeltaValue <= 0.f) return;
+
+	Value += DeltaValue;
+
+	if (bClampToMax)
+	{
+		Value = FMath::Min(Value, ValueMax);
+	}
+
+	OnValueIncreased.Broadcast(Value);
+}
+
+void UResource::DecreaseValueMax(const float DeltaValue, const bool bClampValue)
+{
+	if (DeltaValue <= 0.f) return;
+
+	ValueMax -= DeltaValue;
+
+	if (bClampValue)
+	{
+		Value = FMath::Min(Value, ValueMax);
+	}
+
+	OnValueMaxChanged.Broadcast();
+}
+
+void UResource::IncreaseValueMax(const float DeltaValue, const bool bClampValue)
+{
+	if (DeltaValue <= 0.f)
+
+		ValueMax += DeltaValue;
+
+	if (bClampValue)
+	{
+		Value = ValueMax;
+	}
+
+	OnValueMaxChanged.Broadcast();
+}
+
+void UResource::SetupResource(const FResourceData ResourceData)
+{
+	Value = ResourceData.bCustomInitialValue ? ResourceData.ValueInitial : ResourceData.ValueMax;
+	ValueMax = ResourceData.ValueMax;
+	bAutoIncreaseEnabled = ResourceData.bAutoIncreaseEnabled;
+	AutoIncreaseThreshold = ResourceData.AutoIncreaseThreshold;
+	AutoIncreaseDelay = ResourceData.AutoIncreaseDelay;
+	SetAutoIncreaseFrequency(ResourceData.AutoIncreaseFrequency);
+	AutoIncreaseValue = ResourceData.AutoIncreaseValue;
+}
+
+void UResource::SetAutoIncreaseEnabled(const bool bIsEnabled, const bool bStopAutoIncrease)
+{
+	if (bAutoIncreaseEnabled == bIsEnabled) return;
+
+	bAutoIncreaseEnabled = bIsEnabled;
+
+	if (!bIsEnabled && bStopAutoIncrease)
+	{
+		UObject* Owner = GetOuter();
+
+		if (!Owner) return;
+		
+		FTimerManager& TimerManager = Owner->GetWorld()->GetTimerManager();
+
+		if (TimerManager.IsTimerActive(AutoIncreaseHandle))
+		{
+			TimerManager.ClearTimer(AutoIncreaseHandle);
+		}
+	}
+}
+
+void UResource::SetAutoIncreaseFrequency(const float NewFrequency)
+{
+	if (NewFrequency <= 0.f) return;
+
+	AutoIncreaseFrequency = NewFrequency;
+	AutoIncreaseTime = 1.f / NewFrequency;
+}
+
+void UResource::StartAutoIncreaseDelay()
+{
+	UObject* Owner = GetOuter();
+
+	if (!Owner) return;
+
+	FTimerManager& TimerManager = Owner->GetWorld()->GetTimerManager();
+
+	if (TimerManager.IsTimerActive(AutoIncreaseHandle))
+	{
+		TimerManager.ClearTimer(AutoIncreaseHandle);
+	}
+
+	TimerManager.SetTimer(AutoIncreaseHandle,
+	                      this,
+	                      &UResource::StartAutoIncrease,
+	                      AutoIncreaseDelay,
+	                      false);
+}
+
+void UResource::StartAutoIncrease()
+{
+	UObject* Owner = GetOuter();
+
+	if (!Owner) return;
+
+	FTimerManager& TimerManager = Owner->GetWorld()->GetTimerManager();
+	TimerManager.SetTimer(AutoIncreaseHandle, this, &UResource::ProcessAutoIncrease, AutoIncreaseTime, true);
+}
+
+void UResource::ProcessAutoIncrease()
+{
+	IncreaseValue(AutoIncreaseValue, false);
+
+	if (Value / ValueMax >= AutoIncreaseThreshold)
+	{
+		UObject* Owner = GetOuter();
+
+		if (!Owner) return;
+
+		FTimerManager& TimerManager = Owner->GetWorld()->GetTimerManager();
+
+		if (TimerManager.IsTimerActive(AutoIncreaseHandle))
+		{
+			TimerManager.ClearTimer(AutoIncreaseHandle);
+		}
+	}
+}
---
+++ Source/ShootThemUp/Private/Objects/Resource.h
@@ -0,0 +1,75 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Resource.generated.h"
+
+USTRUCT(BlueprintType)
+struct FResourceData
+{
+	GENERATED_USTRUCT_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ValueMax = 100.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bCustomInitialValue = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(EditCondition="bCustomInitialValue"))
+	float ValueInitial = 100.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bAutoIncreaseEnabled = false;
+	UPROPERTY(EditAnywhere,
+		BlueprintReadWrite,
+		meta=(EditCondition="bAutoIncreaseEnabled", ClampMin="0.0", ClampMax="1.0"))
+	float AutoIncreaseThreshold = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(EditCondition="bAutoIncreaseEnabled", ClampMin="0.0"))
+	float AutoIncreaseDelay = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(EditCondition="bAutoIncreaseEnabled", ClampMin="0.0"))
+	float AutoIncreaseFrequency = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(EditCondition="bAutoIncreaseEnabled", ClampMin="0.0"))
+	float AutoIncreaseValue = 1.f;
+};
+
+/**
+ * 
+ */
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnValueIncreased, float)
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnValueDecreased, float)
+DECLARE_MULTICAST_DELEGATE(FOnValueMaxChanged);
+
+UCLASS()
+class SHOOTTHEMUP_API UResource : public UObject
+{
+	GENERATED_BODY()
+public:
+	float GetValue() const { return Value; }
+	void SetValue(const float NewValue);
+	float GetValueMax() const { return ValueMax; }
+	void SetValueMax(const float NewValue);
+	void DecreaseValue(const float DeltaValue, const bool bClampToZero);
+	void IncreaseValue(const float DeltaValue, const bool bClampToMax);
+	void DecreaseValueMax(const float DeltaValue, const bool bClampValue);
+	void IncreaseValueMax(const float DeltaValue, const bool bClampValue);
+	void SetupResource(const FResourceData ResourceData);
+	void SetAutoIncreaseEnabled(const bool bIsEnabled, const bool bStopAutoIncrease = true);
+	void SetAutoIncreaseFrequency(const float NewFrequency);
+	FOnValueIncreased OnValueIncreased;
+	FOnValueDecreased OnValueDecreased;
+	FOnValueMaxChanged OnValueMaxChanged;
+
+protected:
+	float Value = 100.f;
+	float ValueMax = 100.f;
+	// Auto increase
+	bool bAutoIncreaseEnabled = false;
+	float AutoIncreaseThreshold = 1.f;
+	float AutoIncreaseDelay = 3.f;
+	float AutoIncreaseFrequency = 1.f;
+	float AutoIncreaseTime = 1.f;
+	float AutoIncreaseValue = 1.f;
+	FTimerHandle AutoIncreaseHandle;
+	void StartAutoIncreaseDelay();
+	void StartAutoIncrease();
+	void ProcessAutoIncrease();
+};
