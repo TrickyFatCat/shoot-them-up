d11f8b2c2d742905389f24646d205863a5333dde
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-20 18:16:31 +0300

    Create base weapon class

---
+++ Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
@@ -0,0 +1,18 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+
+#include "Weapon/STUBaseWeapon.h"
+#include "Components/SkeletalMeshComponent.h"
+
+ASTUBaseWeapon::ASTUBaseWeapon()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponMesh");
+    SetRootComponent(WeaponMesh);
+}
+
+void ASTUBaseWeapon::BeginPlay()
+{
+    Super::BeginPlay();
+}
---
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -0,0 +1,24 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUBaseWeapon.generated.h"
+
+class USkeletalMeshComponent;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUBaseWeapon : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ASTUBaseWeapon();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Component", meta=(AllowPrivateAccess="true"))
+    USkeletalMeshComponent* WeaponMesh = nullptr;
+};
