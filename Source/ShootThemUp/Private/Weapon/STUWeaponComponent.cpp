82ce8c6265ba2900b1d116d59ec7fc891b40eca6
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-21 17:19:00 +0300

    Create WeaponController class

---
+++ Source/ShootThemUp/Private/Weapon/STUWeaponComponent.cpp
@@ -0,0 +1,39 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+
+#include "Weapon/STUWeaponComponent.h"
+#include "Weapon/STUBaseWeapon.h"
+#include "GameFramework/Character.h"
+
+
+USTUWeaponComponent::USTUWeaponComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void USTUWeaponComponent::Fire() const
+{
+    if (!CurrentWeapon) return;
+
+    CurrentWeapon->Fire();
+}
+
+void USTUWeaponComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Owner = Cast<ACharacter>(GetOwner());
+    SpawnWeapon();
+}
+
+void USTUWeaponComponent::SpawnWeapon()
+{
+    if (!GetWorld() || !Owner) return;
+
+    CurrentWeapon = GetWorld()->SpawnActor<ASTUBaseWeapon>(WeaponClass);
+
+    if (!CurrentWeapon) return;
+    
+    const FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, false);
+    CurrentWeapon->AttachToComponent(Owner->GetMesh(), AttachmentRules, WeaponSocketName);
+}
