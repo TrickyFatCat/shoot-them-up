ded39eb3d28a2d691cad11a5091c160588f57ca2
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-25 22:06:10 +0300

    Implement projectile movement

--- Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
+++ Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
@@ -3,6 +3,7 @@
 
 #include "Weapon/STUProjectile.h"
 #include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 ASTUProjectile::ASTUProjectile()
 {
@@ -11,10 +12,18 @@ ASTUProjectile::ASTUProjectile()
     CollisionComponent = CreateDefaultSubobject<USphereComponent>("CollisionComponent");
     CollisionComponent->InitSphereRadius(5.0f);
     SetRootComponent(CollisionComponent);
+
+    MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("MovementComponent");
+    MovementComponent->InitialSpeed = 2000.f;
+    MovementComponent->ProjectileGravityScale = 0.f;
 }
 
 void ASTUProjectile::BeginPlay()
 {
     Super::BeginPlay();
+
+    check(MovementComponent);
+    MovementComponent->Velocity = ShotDirection * MovementComponent->InitialSpeed;
+    SetLifeSpan(5.f);
 }
 
--- Source/ShootThemUp/Public/Weapon/STUProjectile.h
+++ Source/ShootThemUp/Public/Weapon/STUProjectile.h
@@ -7,6 +7,7 @@
 #include "STUProjectile.generated.h"
 
 class USphereComponent;
+class UProjectileMovementComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUProjectile : public AActor
@@ -15,9 +16,15 @@ class SHOOTTHEMUP_API ASTUProjectile : public AActor
 
 public:
     ASTUProjectile();
+    void SetShotDirection(const FVector& Direction) { ShotDirection = Direction; } 
 
 protected:
     virtual void BeginPlay() override;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
     USphereComponent* CollisionComponent = nullptr;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
+    UProjectileMovementComponent* MovementComponent = nullptr;
+
+private:
+    FVector ShotDirection = FVector::ZeroVector;
 };
