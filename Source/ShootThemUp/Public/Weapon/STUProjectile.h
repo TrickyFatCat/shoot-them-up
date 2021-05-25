641ab40dde0c76fc825e1e997d131532422bd112
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-25 20:10:23 +0300

    Create projectile class

---
+++ Source/ShootThemUp/Public/Weapon/STUProjectile.h
@@ -0,0 +1,23 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUProjectile.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUProjectile : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ASTUProjectile();
+
+protected:
+    virtual void BeginPlay() override;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
+    USphereComponent* CollisionComponent = nullptr;
+};
