9caf2f672ac8af9a3d9fad11ea11b53d4ffcd3df
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-27 20:36:38 +0300

    Create custom AnimNotify classes

---
+++ Source/ShootThemUp/Public/Animations/STUEquipFinishedAnimNotify.h
@@ -0,0 +1,22 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "STUEquipFinishedAnimNotify.generated.h"
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnNotifiedSignature, USkeletalMeshComponent*)
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUEquipFinishedAnimNotify : public UAnimNotify
+{
+    GENERATED_BODY()
+
+public:
+    virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+    FOnNotifiedSignature OnNotified;
+};
