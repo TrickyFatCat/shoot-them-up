3bd1136ee61e7c9550eb5f920fd8ba4d21323efd
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-05-28 19:22:29 +0300

    Create anim notify class for reloading

---
+++ Source/ShootThemUp/Public/Animations/STUReloadAnimNotify.h
@@ -0,0 +1,17 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animations/STUAnimNotify.h"
+#include "STUReloadAnimNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUReloadAnimNotify : public USTUAnimNotify
+{
+	GENERATED_BODY()
+	
+};
