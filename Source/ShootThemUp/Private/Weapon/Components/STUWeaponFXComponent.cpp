f0df2932698d1c61d098584da4c76bc964bcf84b
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-06-08 19:27:03 +0300

    Create component for playing weapon FX

---
+++ Source/ShootThemUp/Private/Weapon/Components/STUWeaponFXComponent.cpp
@@ -0,0 +1,31 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+
+#include "Weapon/Components/STUWeaponFXComponent.h"
+#include "NiagaraFunctionLibrary.h"
+
+
+USTUWeaponFXComponent::USTUWeaponFXComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void USTUWeaponFXComponent::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+
+void USTUWeaponFXComponent::TickComponent(float DeltaTime,
+                                          ELevelTick TickType,
+                                          FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void USTUWeaponFXComponent::PlayImpactFX(const FHitResult& Hit)
+{
+    UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), ImpactEffect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
+}
+
---
+++ Source/ShootThemUp/Public/Weapon/Components/STUWeaponFXComponent.h
@@ -0,0 +1,33 @@
+﻿// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "STUWeaponFXComponent.generated.h"
+
+class UNiagaraSystem;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SHOOTTHEMUP_API USTUWeaponFXComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    USTUWeaponFXComponent();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void TickComponent(float DeltaTime,
+                               ELevelTick TickType,
+                               FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+    void PlayImpactFX(const FHitResult& Hit);
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="VFX")
+    UNiagaraSystem* ImpactEffect;
+};
