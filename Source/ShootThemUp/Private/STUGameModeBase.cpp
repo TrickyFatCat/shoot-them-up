e7aa3fefd78d3053edb3cb2adfbf20ef7d2b042d
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-07-19 22:02:09 +0300

    Update the base game mode

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -5,10 +5,41 @@
 #include "Characters/STUBaseCharacter.h"
 #include "Characters/Controllers/STUPlayerController.h"
 #include "UI/STUGameHUD.h"
+#include "STUCoreTypes.h"
+#include "AIController.h"
 
 ASTUGameModeBase::ASTUGameModeBase()
 {
     DefaultPawnClass = ASTUBaseCharacter::StaticClass();
     PlayerControllerClass = ASTUPlayerController::StaticClass();
     HUDClass = ASTUGameHUD::StaticClass();
+}
+
+void ASTUGameModeBase::StartPlay()
+{
+    Super::StartPlay();
+    SpawnBots();
+}
+
+UClass* ASTUGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+    if(InController && InController->IsA<AAIController>())
+    {
+        return AIPawnClass;
+    }
+
+    return AIPawnClass;
+}
+
+void ASTUGameModeBase::SpawnBots()
+{
+   if (!GetWorld()) return;
+
+    for(int32 i = 0; i < GameData.PlayersNumber - 1; ++i)
+    {
+        FActorSpawnParameters SpawnInfo;
+        SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+        AAIController* STUAIController = GetWorld()->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+        RestartPlayer(STUAIController);
+    }
 }
