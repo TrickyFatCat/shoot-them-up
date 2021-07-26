2b2edefbc7662a9760fcbb12e560511e527e8463
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-07-26 21:58:41 +0300

    Create new widget cpp class

---
+++ Source/ShootThemUp/Private/UI/STUGameDataWidget.cpp
@@ -0,0 +1,40 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+
+#include "UI/STUGameDataWidget.h"
+#include "STUGameModeBase.h"
+#include "Characters/STUPlayerState.h"
+
+int32 USTUGameDataWidget::GetKills() const
+{
+    ASTUPlayerState* PlayerState = GetSTUPlayerState();
+    return PlayerState ? PlayerState->GetKillsNumber() : -1;
+}
+
+int32 USTUGameDataWidget::GetCurrentRoundNum() const
+{
+    ASTUGameModeBase* GameMode = GetSTUGameMode();
+    return GameMode ? GameMode->GetCurrentRoundNum() : -1;
+}
+
+int32 USTUGameDataWidget::GetTotalRoundsNum() const
+{
+    ASTUGameModeBase* GameMode = GetSTUGameMode();
+    return GameMode ? GameMode->GetGameData().RoundsNumber : -1;
+}
+
+int32 USTUGameDataWidget::GetRoundSecondsRemaining() const
+{
+    ASTUGameModeBase* GameMode = GetSTUGameMode();
+    return GameMode ? GameMode->GetRoundSecondsRemaining() : -1;
+}
+
+ASTUGameModeBase* USTUGameDataWidget::GetSTUGameMode() const
+{
+    return GetWorld() ? Cast<ASTUGameModeBase>(GetWorld()->GetAuthGameMode()) : nullptr;
+}
+
+ASTUPlayerState* USTUGameDataWidget::GetSTUPlayerState() const
+{
+    return GetOwningPlayer() ? Cast<ASTUPlayerState>(GetOwningPlayer()->PlayerState) : nullptr;
+}
