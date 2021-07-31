c042045a4aa70a692c2e94644e5178ec01262ab7
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-07-31 20:05:21 +0300

    Create player statistics widget class

---
+++ Source/ShootThemUp/Private/UI/STUPlayerStatRowWidget.cpp
@@ -0,0 +1,41 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+
+#include "UI/STUPlayerStatRowWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+
+void USTUPlayerStatRowWidget::SetPlayerName(const FText& Text) const
+{
+    if (!TextPlayerName) return;
+
+    TextPlayerName->SetText(Text);
+}
+
+void USTUPlayerStatRowWidget::SetKills(const FText& Text) const
+{
+    if (!TextKills) return;
+
+    TextKills->SetText(Text);
+}
+
+void USTUPlayerStatRowWidget::SetDeaths(const FText& Text) const
+{
+    if (!TextDeaths) return;
+
+    TextDeaths->SetText(Text);
+}
+
+void USTUPlayerStatRowWidget::SetTeam(const FText& Text) const
+{
+    if (!TextTeam) return;
+
+    TextTeam->SetText(Text);
+}
+
+void USTUPlayerStatRowWidget::SetPlayerIndicatorVisibility(const bool bIsVisible) const
+{
+    if (!ImagePlayerIndicator) return;
+
+    ImagePlayerIndicator->SetVisibility(bIsVisible ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+}
---
+++ Source/ShootThemUp/Public/UI/STUPlayerStatRowWidget.h
@@ -0,0 +1,42 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "STUPlayerStatRowWidget.generated.h"
+
+class UImage;
+class UTextBlock;
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUPlayerStatRowWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    void SetPlayerName(const FText& Text) const;
+    void SetKills(const FText& Text) const;
+    void SetDeaths(const FText& Text) const;
+    void SetTeam(const FText& Text) const;
+    void SetPlayerIndicatorVisibility(const bool bIsVisible) const;
+
+protected:
+    UPROPERTY(meta=(BindWidget))
+    UTextBlock* TextPlayerName = nullptr;
+    
+    UPROPERTY(meta=(BindWidget))
+    UTextBlock* TextKills = nullptr;
+    
+    UPROPERTY(meta=(BindWidget))
+    UTextBlock* TextDeaths = nullptr;
+    
+    UPROPERTY(meta=(BindWidget))
+    UTextBlock* TextTeam = nullptr;
+    
+    UPROPERTY(meta=(BindWidget))
+    UImage* ImagePlayerIndicator = nullptr;
+};
