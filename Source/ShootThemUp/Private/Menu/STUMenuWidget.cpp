18bfba76684c671a5f8c626c6a5f047e6826e806
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-08-02 20:47:38 +0300

    Update menu widget class

--- Source/ShootThemUp/Private/Menu/STUMenuWidget.cpp
+++ Source/ShootThemUp/Private/Menu/STUMenuWidget.cpp
@@ -2,4 +2,21 @@
 
 
 #include "Menu/STUMenuWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
 
+void USTUMenuWidget::NativeOnInitialized()
+{
+    Super::NativeOnInitialized();
+
+    if (ButtonStartGame)
+    {
+        ButtonStartGame->OnClicked.AddDynamic(this, &USTUMenuWidget::OnStartGame);
+    }
+}
+
+void USTUMenuWidget::OnStartGame()
+{
+    const FName StartLevelName = "Test";
+    UGameplayStatics::OpenLevel(this, StartLevelName);
+}
--- Source/ShootThemUp/Public/Menu/STUMenuWidget.h
+++ Source/ShootThemUp/Public/Menu/STUMenuWidget.h
@@ -6,12 +6,23 @@
 #include "Blueprint/UserWidget.h"
 #include "STUMenuWidget.generated.h"
 
+class UButton;
+
 /**
  * 
  */
 UCLASS()
 class SHOOTTHEMUP_API USTUMenuWidget : public UUserWidget
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+protected:
+    virtual void NativeOnInitialized() override;
+    
+    UPROPERTY(meta=(BindWidget))
+    UButton* ButtonStartGame = nullptr;
+
+private:
+    UFUNCTION()
+    void OnStartGame();
 };
