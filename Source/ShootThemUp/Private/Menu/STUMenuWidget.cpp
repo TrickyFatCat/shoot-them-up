e3e500565fec14a0fd06b692ef6d2f013a757706
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-08-03 23:43:14 +0300

    Implement level selection to menu

--- Source/ShootThemUp/Private/Menu/STUMenuWidget.cpp
+++ Source/ShootThemUp/Private/Menu/STUMenuWidget.cpp
@@ -6,6 +6,9 @@
 #include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "STUGameInstance.h"
+#include "STUCoreTypes.h"
+#include "Components/HorizontalBox.h"
+#include "Menu/STUButtonLevelSelectWidget.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogMenuWidget, All, All);
 
@@ -22,29 +25,84 @@ void USTUMenuWidget::NativeOnInitialized()
     {
         ButtonQuitGame->OnClicked.AddDynamic(this, &USTUMenuWidget::OnQuitGame);
     }
+
+    InitLevelSelectButtons();
 }
 
 void USTUMenuWidget::OnStartGame()
 {
-    UWorld* World = GetWorld();
-
-    if (!World) return;
-
-    USTUGameInstance* GameInstance = World->GetGameInstance<USTUGameInstance>();
+    USTUGameInstance* GameInstance = GetGameInstance();
 
     if (!GameInstance) return;
 
-    const FName StartLevelName = GameInstance->GetStartLevelName();
-    
-    if (StartLevelName.IsNone())
-    {
-        UE_LOG(LogMenuWidget, Error, TEXT("Start level name is NONE") )
-    }
-    
-    UGameplayStatics::OpenLevel(this, StartLevelName);
+    UGameplayStatics::OpenLevel(this, GameInstance->GetStartLevelData().LevelName);
 }
 
 void USTUMenuWidget::OnQuitGame()
 {
     UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, true);
+}
+
+void USTUMenuWidget::InitLevelSelectButtons()
+{
+    const USTUGameInstance* GameInstance = GetGameInstance();
+
+    if (!GameInstance) return;
+
+    checkf(GameInstance->GetLevelsData().Num() != 0, TEXT("Levels data must not be empty!"));
+
+    if (!BoxLevelsSelectors) return;
+
+    BoxLevelsSelectors->ClearChildren();
+
+    for (auto LevelData : GameInstance->GetLevelsData())
+    {
+        USTUButtonLevelSelectWidget* LevelSelectButton = CreateWidget<USTUButtonLevelSelectWidget>(
+            GetWorld(),
+            SelectButtonWidgetClass);
+
+        if (!LevelSelectButton) continue;
+
+        LevelSelectButton->SetLevelData(LevelData);
+        LevelSelectButton->OnLevelSelected.AddUObject(this, &USTUMenuWidget::OnLevelSelected);
+
+        BoxLevelsSelectors->AddChild(LevelSelectButton);
+        LevelSelectButtons.Add(LevelSelectButton);
+    }
+
+    if (GameInstance->GetStartLevelData().LevelName.IsNone())
+    {
+        OnLevelSelected(GameInstance->GetLevelsData()[0]);
+    }
+    else
+    {
+        OnLevelSelected(GameInstance->GetStartLevelData());
+    }
+}
+
+void USTUMenuWidget::OnLevelSelected(const FLevelData& Data)
+{
+    USTUGameInstance* GameInstance = GetGameInstance();
+
+    if (!GameInstance) return;
+
+    GameInstance->SetStartLevelData(Data);
+
+    for(auto LevelButton : LevelSelectButtons)
+    {
+        if (LevelButton)
+        {
+            const bool bIsSelected = Data.LevelName == LevelButton->GetLevelData().LevelName;
+            LevelButton->SetSelected(bIsSelected);
+        }
+    }
+}
+
+USTUGameInstance* USTUMenuWidget::GetGameInstance() const
+{
+    UWorld* World = GetWorld();
+
+    if (!World) return nullptr;
+
+    return World->GetGameInstance<USTUGameInstance>();
 }
--- Source/ShootThemUp/Public/Menu/STUMenuWidget.h
+++ Source/ShootThemUp/Public/Menu/STUMenuWidget.h
@@ -4,9 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "STUCoreTypes.h"
 #include "STUMenuWidget.generated.h"
 
 class UButton;
+class UHorizontalBox;
+class USTUGameInstance;
+class USTUButtonLevelSelectWidget;
 
 /**
  * 
@@ -24,11 +28,24 @@ protected:
     
     UPROPERTY(meta=(BindWidget))
     UButton* ButtonQuitGame = nullptr;
+    
+    UPROPERTY(meta=(BindWidget))
+    UHorizontalBox* BoxLevelsSelectors = nullptr;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="UI")
+    TSubclassOf<UUserWidget> SelectButtonWidgetClass;
 
 private:
+    UPROPERTY()
+    TArray<USTUButtonLevelSelectWidget*>  LevelSelectButtons;
+    
     UFUNCTION()
     void OnStartGame();
 
     UFUNCTION()
     void OnQuitGame();
+
+    void InitLevelSelectButtons();
+    void OnLevelSelected(const FLevelData& Data);
+    USTUGameInstance* GetGameInstance() const;
 };
