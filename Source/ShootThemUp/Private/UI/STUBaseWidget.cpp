fede9c88839a0e0c28512c162b53657a10a3ccd2
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-08-08 23:52:49 +0300

    Create base widget class

---
+++ Source/ShootThemUp/Private/UI/STUBaseWidget.cpp
@@ -0,0 +1,9 @@
+// A simple Shoot Them Up game made during UE4 C++ course. All rights reserved.
+
+
+#include "UI/STUBaseWidget.h"
+
+void USTUBaseWidget::Show()
+{
+    PlayAnimation(ShowAnimation);
+}
